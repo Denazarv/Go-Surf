06db3f7bd5d201952f0f29c18b8795b185f423ca
Denis Azarov <dnsaav@gmail.com>
2021-10-23 00:31:47 +0300

    start

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,16 @@
-$(function(){
+$(function () {
+  $(".header__slider").slick({
+    infinite: true,
+    fade: true,
+    prevArrow: '<img class ="slider-arrow slider-arrow__left"src="img/arrow-left.svg" alt="move to left">',
+    nextArrow: '<img class ="slider-arrow slider-arrow__right"src="img/arrow-right.svg" alt="move to right">',
+    asNavFor: '.slider-dots',
+  });
 
-
-
+$('.slider-dots').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    asNavFor: '.header__slider',
+});
   
 });
